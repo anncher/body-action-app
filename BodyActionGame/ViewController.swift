a062fbfebf4fbf93f98192fb7736adfe6acf1c62
Anncher <hczerkesowa@gmail.com>
2023-08-25 15:11:23 -0500

    elements randomisation

--- BodyActionGame/ViewController.swift
+++ BodyActionGame/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var leftButton: UIImageView!
     @IBOutlet weak var rightButton: UIImageView!
-    @IBOutlet weak var rollButton: UIButton!
+    @IBAction func rollButton(_ sender: Any) {
+        
+        let leftButtonArray = [#imageLiteral(resourceName: "suckAction"), #imageLiteral(resourceName: "kissAct"), #imageLiteral(resourceName: "massageAct"), #imageLiteral(resourceName: "tickleAct"), #imageLiteral(resourceName: "blowAct"), #imageLiteral(resourceName: "lickAct"), #imageLiteral(resourceName: "touchAct"), #imageLiteral(resourceName: "yourChoiceAct")]
+        let rightButtonArray = [#imageLiteral(resourceName: "earsBody"), #imageLiteral(resourceName: "nipplesBody"), #imageLiteral(resourceName: "thighBody"), #imageLiteral(resourceName: "handBody"), #imageLiteral(resourceName: "lipsBody"), #imageLiteral(resourceName: "neckBody"), #imageLiteral(resourceName: "fingersBody"), #imageLiteral(resourceName: "yourChoiceBody")]
+        
+        leftButton.image = leftButtonArray.randomElement()
+        rightButton.image = rightButtonArray.randomElement()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
