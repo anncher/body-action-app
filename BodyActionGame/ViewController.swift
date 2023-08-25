33782cd5640c0d59b06b5f4eb319ebd5a869b20d
Anncher <hczerkesowa@gmail.com>
2023-08-25 12:31:07 -0500

    UI elements added

--- BodyActionGame/ViewController.swift
+++ BodyActionGame/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftButton: UIImageView!
+    @IBOutlet weak var rightButton: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
